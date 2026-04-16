55a529c84d1ba77ade02d55f8822663b1d9910df
Zihro <skylerfowler03@gamil.com>
2026-04-16 11:41:17 -0700

    New Attemps Backported

---
+++ src/main/java/net/zihro/zihropoint/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.zihro.zihropoint.item;
+
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.zihro.zihropoint.ZihroPoint;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ZihroPoint.MOD_ID);
+
+    public static final RegistryObject<Item> RAW_NICKEL =ITEMS.register("raw_nickel",
+            () -> new Item(new Item.Properties()));
+
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+
+}
